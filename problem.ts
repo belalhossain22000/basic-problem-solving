cbf94bce299e1495021059d267c9c36915228e82
SUMON <belalhossain22000@gmail.com>
2024-01-06 00:58:30 +0600

    added all question

--- problem.ts
+++ problem.ts
@@ -1,4 +1,7 @@
 // problem-1
+/**
+ * Design a TypeScript function that takes a parameter of a union type (e.g., string | number). If it's a string, return its length. If it's a number, return the square of that number.
+*/
 const checkType = (input: string | number): number => {
     if (typeof input == 'string') {
         return input.length
@@ -11,6 +14,9 @@ const checkType = (input: string | number): number => {
 
 
 //problem-2
+/***
+ * Create an interface called Person with optional properties address and phone. The address property itself will be another object containing city and street properties. Implement a function named getAddressCity that takes an argument of type Person and returns the city from the address property of the Person object. Use optional chaining to prevent any type errors.
+ */
 
 interface Address {
     city?: string;
@@ -29,6 +35,9 @@ const getAddressCity = (person: Person): string | undefined => {
 
 
 //problem-3
+/**
+ * Create a type guard function isCat that checks if an object is an instance of a Cat class. If it does, the function says "yes, it's a cat." If it doesn't match, the function says "no, it's not a cat."
+ */
 
 class Cat {
     name: string;
@@ -50,6 +59,13 @@ const isCat = (cat: any): string => {
 }
 
 //problem-4
+/**
+ * You got a list that has numbers and words mixed together. Your job is to make a function that will take the list as an argument and return the total by adding them up.
+
+To solve this in TypeScript, you'll look at each thing in the list named mixedData, which looks like this: [1, 'two', 3, 'four', 5]. You'll go through the list, find the numbers, and add them together.
+
+Every time you find a number in the list, you'll add it to a total. You'll start at zero and then keep adding the numbers you find. If no number is found in the list return 0. To make sure TypeScript knows these things are numbers, you'll use type assertions.
+ */
 
 const findNumberSum = <T>(mixedData: T[]): number => {
     let total = 0;
@@ -65,6 +81,9 @@ const findNumberSum = <T>(mixedData: T[]): number => {
 
 
 //problem-5
+/**
+ * Define two interfaces: Car with properties like make, model, and year, and Driver with properties like name and licenseNumber. Create a function that takes two objects of type Car and Driver and returns an object with the combined properties of both types.
+ */
 
 interface Car {
     make: string;
@@ -86,7 +105,9 @@ const objectCombined = <Tc, Td>(car: Tc, driver: Td): Tc & Td => {
 
 
 //problem-6
-
+/**
+ * Write a TypeScript function that takes a parameter of type unknown and uses a type guard to check whether the parameter is an array of numbers. If it is, calculate the sum of the numbers and log it. If it's not, log an error message.
+ */
 const calculateSumFromArray = (param: unknown): void => {
     if (Array.isArray(param) && param.every((item) => typeof item === 'number')) {
         const sum = (param as number[]).reduce((acc, val) => acc + val, 0);
@@ -97,6 +118,27 @@ const calculateSumFromArray = (param: unknown): void => {
 }
 
 //problem-7
+/**
+ * Create a TypeScript function called findFirstOccurrence that accepts an array and a value to search for. Use generics to make the function work with arrays of any data type. Inside the function, find and return the index of the first occurrence of the value in the array. If the value is not found in the array, return -1 to indicate that. Create sample arrays of different data types (e.g., numbers, strings) and call the findFirstOccurrence function for each of them to display the results.
+
+// Example usage:
+
+const numbers: number[] = [1, 2, 3, 4, 5, 2];
+
+const strings: string[] = ["apple", "banana", "cherry", "date", "apple"];
+
+const targetNumber = 2;
+
+const targetString = "cherry";
+
+const indexInNumbers = findFirstOccurrence(numbers, targetNumber);
+
+const indexInStrings = findFirstOccurrence(strings, targetString);
+
+console.log(indexInNumbers); //output:  1
+
+console.log(indexInStrings); //output: 2
+ */
 
 const findFirstOccurrence = <T>(arr: T[], value: T): number => {
     const index = arr.findIndex((item) => item === value);
